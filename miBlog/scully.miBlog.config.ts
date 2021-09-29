9725fe5ad1879fb9c2a1b24719ebd2fd48978845
BrandonLau1 <lauvalenzuelabrandon@gmail.com>
2021-09-29 15:58:04 -0600

    29/09/2021

---
+++ miBlog/scully.miBlog.config.ts
@@ -0,0 +1,20 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "miBlog",
+  outDir: './dist/static',
+  routes: {
+    '/posts/:id': {
+      type: 'contentFolder',
+      id: {
+        folder: "./posts"
+      }
+    },
+    '/blog/:id': {
+      type: 'contentFolder',
+      id: {
+        folder: "./blog"
+      }
+    },
+  }
+};
