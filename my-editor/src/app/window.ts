f1fcbbddf7c2b14bf3201651da7a14865437d833
BrandonLau1 <lauvalenzuelabrandon@gmail.com>
2021-12-03 14:51:04 -0700

    prueba

--- my-editor/src/app/window.ts
+++
@@ -1,9 +0,0 @@
-import { InjectionToken } from "@angular/core";
-export const WINDOW = new
-    InjectionToken<Window>('Global window object', {
-        factory: () => window
-    });
-
-export interface ElectronWindow extends Window {
-    require(module: string): any;
-}
